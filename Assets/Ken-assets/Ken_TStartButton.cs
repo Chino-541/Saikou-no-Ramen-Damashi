5c2aff4dd07fb92940d22eb44ec19309752877cd
5F\ktc <ktc25a31a0036@edu.kyoto-tech.ac.jp>
2026-03-13 16:26:42 +0900

    settei

--- Assets/Ken-assets/Ken_TStartButton.cs
+++ Assets/Ken-assets/Ken_TStartButton.cs
@@ -6,15 +6,13 @@ using UnityEngine.UI;
 public class Ken_TStartButton : MonoBehaviour
 {
     [Header("Nyanスクリプトへようこそ")]
-    private Button _SButton;
+    [SerializeField] private Button _SButton;
     [Scene]
-    [SerializeField] private string _Scene;
+    [SerializeField] private int _Scene;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _SButton = GetComponent<Button>();
         _SButton.onClick.AddListener(StartButton);
-
     }
 
     // Update is called once per frame
@@ -25,8 +23,9 @@ public class Ken_TStartButton : MonoBehaviour
     
     void StartButton()
     {
-        Debug.Log("aa");
         SceneManager.LoadScene(_Scene);
 
     }
+    
+    
 }
