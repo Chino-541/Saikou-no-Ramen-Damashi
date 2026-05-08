89e2023535e221be0b89ac40cbb3f8d9155b9580
ke-ta0 <ktc25a31a0039@edu.kyoto-tech.ac.jp>
2026-05-08 19:48:18 +0900

    tilemap

--- Assets/Keita-assets/script/harvest.cs
+++ Assets/Keita-assets/script/harvest.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class harvest : MonoBehaviour
 {
+    [SerializeField] private GameObject Vegetable;
+    [SerializeField] private AnimatedTile animatedTile;
+    [SerializeField] private Tilemap tilemap;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +16,15 @@ public class harvest : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+       if(collision.gameObject.CompareTag("Player") && Input.GetKey(KeyCode. F))
+        {
+            Destroy(gameObject);
+            Vector3Int cellPos = tilemap.WorldToCell(transform.position);
+            tilemap.SetTile(cellPos, animatedTile);
+            Debug.Log("byebye");
+        }
     }
 }
