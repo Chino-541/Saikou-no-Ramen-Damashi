0a8eb88c01ece0b93917305cd0f037c4c5a0c207
ke-ta0 <kujingsan@gmail.com>
2026-05-15 19:45:55 +0900

    メニューセレクトのスクリプト

--- Assets/Tomoki-assets/UIInvantoryItem.cs
+++ Assets/Tomoki-assets/UIInvantoryItem.cs
@@ -1,16 +1,85 @@
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.LowLevel;
 
 public class UIInvantoryItem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private Image itemImage;
+    [SerializeField]
+    private TMP_Text quantityTxt;
+
+    [SerializeField]
+    private Image borderImage;
+
+    public event Action<UIInvantoryItem> OnItemClicked,
+                                         OnItemDroppedOn,
+                                         OnItemBeginDrag,
+                                         OnItemEndDrag,
+                                         OnRightItemMouseBtnClick;
+    private bool empty = true;
+
+    public void Awake()
     {
-        
+        ResetData();
+        Deselect();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResetData()
     {
-        
+        this.itemImage.gameObject.SetActive(false);
+        empty = true;
+    }
+
+    public void Deselect()
+    {
+        borderImage.enabled = false;
+    }
+
+    public void SetData(Sprite sprite, int quantity)
+    {
+        this.itemImage.gameObject.SetActive(true);
+        this.itemImage.sprite = sprite;
+        this.quantityTxt.text = quantity + "";
+        empty = false;
+    }
+
+    public void Select()
+    {
+        borderImage.enabled = true;
+    }
+
+    public void OnBegindrag()
+    {
+        if (empty) return;
+        OnItemBeginDrag?.Invoke(this);
+    }
+
+    public void OnDrop()
+    {
+        OnItemDroppedOn?.Invoke(this);
+    }
+
+    public void OnEndDrag()
+    {
+        OnItemEndDrag?.Invoke(this);
+    }
+
+    public void OnPointerClick(BaseEventData data)
+    {
+        PointerEventData pointerData = (PointerEventData)data;
+        if (pointerData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightItemMouseBtnClick?.Invoke(this);
+        }
+        else
+        {
+            OnItemClicked?.Invoke(this);
+        }
     }
 }
