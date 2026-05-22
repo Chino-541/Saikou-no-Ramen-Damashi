7e2cf85499f0fd654d9f4accc3fa12e74ac5df72
yuurururu <kujingsan@gmail.com>
2026-05-22 19:41:09 +0900

    材料のステータスをいじれるようにした。

--- Assets/Hori-assets/script/Item/ItemData.cs
+++ Assets/Hori-assets/script/Item/ItemData.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewItem", menuName = "Item/Create Item")]
 public class ItemData : ScriptableObject
 {
+    [Header("基本情報")]
     public string itemName;
 
     public Sprite icon;
@@ -11,4 +12,15 @@ public class ItemData : ScriptableObject
     public string description;
 
     public int maxStack = 99;
+
+    [Header("味ステータス")]
+    public int saltiness; // 塩分
+
+    public int umami; // 旨味
+
+    public int spiciness; // 辛さ
+
+    public int fatness; // 肉脂
+
+    public int mystery; // 神秘
 }
