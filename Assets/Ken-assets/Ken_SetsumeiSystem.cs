cbe53d0960295bb75e163646357be47d61b0e3e1
ke-ta0 <ktc25a31a0039@edu.kyoto-tech.ac.jp>
2026-06-07 22:02:54 +0900

    Revert "Merge remote-tracking branch 'origin/ken'"
    This reverts commit 5015e9b46272f126355ee32ce94636b74c0e713a, reversing
    changes made to 61a4a16657aa846a986e5753483e7e88ee46f07c.

--- Assets/Ken-assets/Ken_SetsumeiSystem.cs
+++ Assets/Ken-assets/Ken_SetsumeiSystem.cs
@@ -63,7 +63,6 @@ public class Ken_SetsumeiSystem : MonoBehaviour
     }
     IEnumerator CharSetsumeiText()
     {
-        /*
         _CharName.text = _Ojiisan;
         _CharName.color = _RamenYaColour;
         _CharImage.sprite = _RamenYaImage.sprite;
@@ -71,52 +70,21 @@ public class Ken_SetsumeiSystem : MonoBehaviour
         yield return StartCoroutine(CharSetsumeiSystem("こんにちは"));
         yield return StartCoroutine(AutoClickText());
         //yield return new WaitForSeconds(_CharTextDelay);
-        */
-        
 
         _CharName.text = _PlayerName;
         _CharName.color = _PlayerColour;
         _CharImage.sprite = _PlayerImage.sprite;
         _CharNaikTurun.SetTrigger("CharNaikTurun");
-        yield return StartCoroutine(CharSetsumeiSystem("店長！やってるかい"));
+        yield return StartCoroutine(CharSetsumeiSystem("こんにちは"));
         yield return StartCoroutine(AutoClickText());
 
         _CharName.text = _Ojiisan;
         _CharName.color = _RamenYaColour;
         _CharImage.sprite = _RamenYaImage.sprite;
         //_CharNaikTurun.SetTrigger("CharNaikTurun");
-        yield return StartCoroutine(CharSetsumeiSystem("。。。"));
+        yield return StartCoroutine(CharSetsumeiSystem("君の名前を教えてくれないかな？"));
         yield return StartCoroutine(AutoClickText());
 
-        _CharName.text = _PlayerName;
-        _CharName.color = _PlayerColour;
-        _CharImage.sprite = _PlayerImage.sprite;
-        yield return StartCoroutine(CharSetsumeiSystem("そういえばアンタはいつもどんな感じでラーメン作ってんだい？"));
-        yield return StartCoroutine(AutoClickText());
-
-        _CharName.text = _PlayerName;
-        _CharName.color = _PlayerColour;
-        _CharImage.sprite = _PlayerImage.sprite;
-        yield return StartCoroutine(CharSetsumeiSystem("常連のよしみでよ、良かったら見せてくれよ"));
-        yield return StartCoroutine(AutoClickText());
-
-        _CharName.text = _Ojiisan;
-        _CharName.color = _RamenYaColour;
-        _CharImage.sprite = _RamenYaImage.sprite;
-        _CharNaikTurun.SetTrigger("CharNaikTurun");
-        yield return StartCoroutine(CharSetsumeiSystem("わかったわ"));
-        yield return StartCoroutine(AutoClickText());
-
-        _CharName.text = _PlayerName;
-        _CharName.color = _PlayerColour;
-        _CharImage.sprite = _PlayerImage.sprite;
-        _CharNaikTurun.SetTrigger("CharNaikTurun");
-        yield return StartCoroutine(CharSetsumeiSystem("よしきた！"));
-        yield return StartCoroutine(AutoClickText());
-
-
-
-        /*
         //-----------------------------------------------------------
         yield return StartCoroutine(PlayerNameIn());
 
@@ -162,8 +130,7 @@ public class Ken_SetsumeiSystem : MonoBehaviour
         yield return StartCoroutine(AutoClickText());
 
         //-----------------------------------------------------------
-        */
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(2);
         SceneManager.LoadScene(_NScene);
       
     }
@@ -269,7 +236,6 @@ public class Ken_SetsumeiSystem : MonoBehaviour
 
         _ChoiceYes.gameObject.SetActive(false);
         _ChoiceNo.gameObject.SetActive(false);
-        //tunggu
         pilihan?.Invoke(_Answer);
     }
     
