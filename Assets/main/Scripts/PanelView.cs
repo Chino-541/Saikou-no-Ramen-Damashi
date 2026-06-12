64c1f3925924f72f141e24b7c7bb56ad7bfa12af
ke-ta0 <ktc25a31a0039@edu.kyoto-tech.ac.jp>
2026-06-12 19:47:33 +0900

    進捗
    釣り中に説明を開けないようにしている途中

--- Assets/main/Scripts/PanelView.cs
+++ Assets/main/Scripts/PanelView.cs
@@ -1,26 +1,45 @@
-using NaughtyAttributes;
 using UnityEngine;
 
 public class PanelView : MonoBehaviour
 {
     [SerializeField] private GameObject panel;
+
+    // 釣りスクリプト参照
+    [SerializeField] private Ken_GFish fishing;
+
     private void Start()
     {
         panel.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Tab))
+        // 釣り中は説明パネルを開けない
+        if (fishing != null && fishing)
         {
-            panel.SetActive(true);
-            Time.timeScale = 0f;
+            if (panel.activeSelf)
+            {
+                panel.SetActive(false);
+                Time.timeScale = 1f;
+            }
+            return;
         }
+
+        RulePanel();
     }
+
     public void Onclickbutton()
     {
         panel.SetActive(false);
         Time.timeScale = 1f;
+    }
+
+    public void RulePanel()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            panel.SetActive(true);
+            Time.timeScale = 0f;
+        }
     }
 }
