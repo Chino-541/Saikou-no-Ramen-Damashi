51d07b9f05d55903ff12701de85b0d1255bfbec3
ke-ta0 <ktc25a31a0039@edu.kyoto-tech.ac.jp>
2026-08-05 21:46:39 +0900

    進捗
    ゲーム開始前のカウントダウンと残り時間が短くなった時の残り秒数をわかりやすくしました。

--- Assets/main/CustomerScripts/GameTimer.cs
+++ Assets/main/CustomerScripts/GameTimer.cs
@@ -1,50 +1,89 @@
 using System.Collections;
 using TMPro;
-using Unity.VectorGraphics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameTimer : MonoBehaviour
 {
-    // 時間
+    // カウントダウン
+    [SerializeField] private float countdownTime = 3;
+    [SerializeField] private TextMeshProUGUI countdownText;
+
+    // ゲーム時間
     [SerializeField] private float timeLimit = 60;
     [SerializeField] private TextMeshProUGUI timeText;
+
     // player関連
     [SerializeField] private Rigidbody2D playerRb;
     [SerializeField] private Ken_PChar player;
-    // シーン遷移の時に使う
+
+    // シーン遷移
     [SerializeField] private string SceneName;
-    // 終了したときの文字
+
+    // 終了時の文字
     [SerializeField] private TextMeshProUGUI finishText;
 
-    private bool isFinished = false; 
-
+    private bool isFinished = false;
+    private bool isCounting = false;
     public System.Action OnTimeUp;
 
     private CustomerSpawner spawner;
 
     private void Start()
     {
+        countdownText.gameObject.SetActive(false);
+        finishText.gameObject.SetActive(false);
+
         spawner = FindObjectOfType<CustomerSpawner>();
         spawner.canSpawn = true;
 
-        StartCoroutine(TimerLoop());
+        CountDown();
     }
 
+    // 開始前カウントダウン
+    void CountDown()
+    {
+        isCounting = true;
+        StartCoroutine(CountdownCoroutine());
+    }
+
+    IEnumerator CountdownCoroutine()
+    {
+        countdownText.gameObject.SetActive(true);
+
+        float countdown = countdownTime;
+        while (countdown > 0)
+        {
+            player.DisableInput(3f);
+            countdownText.text = countdown.ToString("F0");
+            yield return new WaitForSeconds(1f);
+            countdown--;
+        }
+
+        countdownText.text = "開始！";
+        yield return new WaitForSeconds(1f);
+
+        countdownText.gameObject.SetActive(false);
+        isCounting = false;
+
+        StartCoroutine(TimerLoop());
+    }
+    // タイマーのコルーチン
     IEnumerator TimerLoop()
     {
+
         while (timeLimit > 0)
         {
             timeLimit--;
             timeText.text = timeLimit.ToString();
 
-            if(timeLimit < 5)
+            // カウントダウンコルーチン開始
+            if (timeLimit == 3)
             {
-                timeText.text = timeLimit.ToString();
-                timeText.color = (timeLimit % 2 == 0) ? Color.red : Color.white;
+                StartCoroutine(LastCountdown());
             }
-            
-            if(timeLimit <= 0)
+            // 終了処理開始
+            if (timeLimit <= 0)
             {
                 timeLimit = 0;
                 FinishTimer();
@@ -60,22 +99,41 @@ public class GameTimer : MonoBehaviour
 
         OnTimeUp?.Invoke();
     }
+    // 残り３秒のカウントダウン
+    IEnumerator LastCountdown()
+    {
+        countdownText.gameObject.SetActive(true);
+
+        float countdown2 = 3;
+        while (countdown2 > 0)
+        {
+            countdownText.text = countdown2.ToString("F0");
+            yield return new WaitForSeconds(1f);
+            countdown2--;
+        }
+
+        countdownText.text = "終了！";
+        yield return new WaitForSeconds(1f);
+
+        countdownText.gameObject.SetActive(false);
+    }
+    // ゲーム終了処理
     void FinishTimer()
     {
         isFinished = true;
 
-        //  時間切れ → Player を停止
         player.DisableInput(9999f);
 
         finishText.gameObject.SetActive(true);
         finishText.text = "終了！";
 
-        StartCoroutine(ChangeSceneAfterDelay());
+        StartCoroutine(ChangeScene());
     }
-
-    private IEnumerator ChangeSceneAfterDelay()
+    // シーン切り替えのコルーチン
+    private IEnumerator ChangeScene()
     {
-         yield return new WaitForSeconds(3f);
+        // 3秒待ってシーン遷移
+        yield return new WaitForSeconds(3f);
         SceneManager.LoadScene(SceneName);
     }
 }
