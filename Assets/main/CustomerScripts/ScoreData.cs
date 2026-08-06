393f12a9f2af7df04722b606dcd453d6f86c84fe
ke-ta0 <ktc25a31a0039@edu.kyoto-tech.ac.jp>
2026-08-06 22:21:49 +0900

    進捗
    リセットできるように

--- Assets/main/CustomerScripts/ScoreData.cs
+++ Assets/main/CustomerScripts/ScoreData.cs
@@ -2,4 +2,10 @@ public static class ScoreData
 {
     public static int point = 0;   // ”Ì”„”
     public static int score = 0;   // ƒXƒRƒA
+
+    public static void ResetScore()
+    {
+        point = 0;
+        score = 0;
+    }
 }
